df89041f8df137530bd64aed6fd407da6cd0fd36
ywh <2453139008@qq.com>
2024-06-26 10:22:15 +0800

    [new] String

---
+++ String/String.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str1;
+	std::clog << "str1: " << str1 << std::endl;
+	std::clog << "str1 is empty: " << (str1.empty() ? "true" : "false") << std::endl;
+	str1.append("Hello, world!");
+	std::clog << "str1: " << str1 << std::endl;
+	std::clog << "str1 is empty: " << (str1.empty() ? "true" : "false") << std::endl;
+	str1.append("Test aaa", 4);
+	std::clog << "str1: " << str1 << std::endl;
+	str1 += " aaa";
+	std::clog << "str1: " << str1 << std::endl;
+	str1.insert(str1.find("Test"), " ");
+	std::clog << "str1: " << str1 << std::endl;
+	str1.replace(str1.find("aaa"), 3, "bbb");
+	std::clog << "str1: " << str1 << std::endl;
+
+	// 查找 Test 在 str1 中的位置
+	std::clog << "Test is at position " << str1.find("Test") << std::endl;
+	// 查找 aaa 在 str1 中的位置
+	std::clog << "aaa is at position:" << str1.find("aaa") << std::endl;
+	std::clog << "ddd is at position:" << (str1.find("ddd") == std::string::npos ? "not found" : "found") << std::endl;
+	std::cin.get();
+}
--- VectorTest/VectorTest.cpp
+++ VectorTest/VectorTest.cpp
@@ -3,11 +3,15 @@
 
 int main()
 {
-	std::vector<int> myVector;
+	std::vector<int> myVector(10, 5);
+	myVector.reserve(100000); // 预分配空间
 
-	myVector.push_back(10);
-	myVector.push_back(20);
-	myVector.push_back(30);
+	std::clog << "size:" << myVector.size() << " capacity:" << myVector.capacity() << std::endl;
+
+	// 巧用swap缩小容量，这是一种优化方式，利用匿名对象，将原向量的容量交换到新向量上，这样就不需要重新分配内存了
+	std::vector<int>(myVector).swap(myVector);
+	std::clog << "size:" << myVector.size() << " capacity:" << myVector.capacity() << std::endl;
+
 
 	for (int i = 0; i < myVector.size(); i++)
 		std::clog << myVector[i] << " ";
@@ -33,16 +37,9 @@ int main()
 
 	// 清空向量
 	myVector.clear();
-	std::clog << "Vector size: " << myVector.size() << std::endl;
+	std::clog << "size: " << myVector.size() << std::endl;
 
-	// 范围循环
-	for (int item : { 1, 2, 3, 4, 5 })
-		std::clog << item << " ";
-	std::clog << std::endl;
 
-	// clog 和 cout 的区别
-	// clog 是 std::clog，是一个 std::ostream 对象，用于输出到控制台，但它是缓冲区，只有缓冲区满了才会输出到控制台。
-	// cout 是 std::cout，是一个 std::ostream 对象，用于输出到控制台，但它是立即输出，即使缓冲区满了也会立即输出。
 
 	std::cin.get();
 }
